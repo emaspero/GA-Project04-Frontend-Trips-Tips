7e41a3fec4d2252ac89dd015f1fc760e94ef2ccf
Elisabetta <emaspero@hotmail.com>
2022-07-19 15:40:16 +0100

    Trip edit functionality

--- src/trips/BrowseTrips.js
+++ src/trips/BrowseTrips.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import TripSnippet from './TripSnippet';
-import TripEditForm from './TripEditForm';
+// import TripEditForm from './TripEditForm';
 
 export default function BrowseTrips() {
   const [trips, setTrips] = useState([]);
   const [currentTrip, setCurrentTrip] = useState("");
-  const [isEdit, setIsEdit] = useState(false);
+  // const [isEdit, setIsEdit] = useState(false);
 
   useEffect(() => {
     loadTripList()
@@ -38,34 +38,34 @@ export default function BrowseTrips() {
   })
   }
 
-  const editView = (id) => {
-    Axios.get(`trip/edit?id=${id}`)
-    .then((response) => {
-      console.log("Loaded trip information for editing", response.data.trip)
-      var trip = response.data.trip
-      setIsEdit(true)
-      setCurrentTrip(trip)
-    })
-    .catch((error) => {
-      console.log("Error loading trip information for editing")
-      console.log(error)
-    })
-  }
+  // const editView = (id) => {
+  //   Axios.get(`trip/edit?id=${id}`)
+  //   .then((response) => {
+  //     console.log("Loaded trip information for editing", response.data.trip)
+  //     var trip = response.data.trip
+  //     setIsEdit(true)
+  //     setCurrentTrip(trip)
+  //   })
+  //   .catch((error) => {
+  //     console.log("Error loading trip information for editing")
+  //     console.log(error)
+  //   })
+  // }
 
-  const editTrip = (trip) => {
-    Axios.put("trip/update", trip)
-    .then((response) => {
-      console.log("Updated trip information")
-    })
-    .catch((error) => {
-      console.log("Error updating trip information")
-      console.log(error)
-    })
-  }
+  // const editTrip = (trip) => {
+  //   Axios.put("trip/update", trip)
+  //   .then((response) => {
+  //     console.log("Updated trip information")
+  //   })
+  //   .catch((error) => {
+  //     console.log("Error updating trip information")
+  //     console.log(error)
+  //   })
+  // }
 
   const allTrips = trips.map((trip, index) => (
     <div key={index}>
-      <TripSnippet {...trip} singleTrip={singleTrip} trip={currentTrip} trips={trips} editView={editView} />
+      <TripSnippet {...trip} singleTrip={singleTrip} trip={currentTrip} trips={trips} />
     </div>
   ));
 
--- src/trips/TripDetail.js
+++ src/trips/TripDetail.js
@@ -1,6 +1,8 @@
 import Axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
+import Trip from './Trip';
+import TripEditForm from './TripEditForm';
 
 
 export default function TripDetail(props) {
@@ -9,6 +11,8 @@ export default function TripDetail(props) {
     // console.log("props trip", props.trip)
 
     const [currentTrip, setCurrentTrip] = useState({});
+    const [isEdit, setIsEdit] = useState(false);
+
 
     useEffect (() => {
         if (id) {
@@ -24,11 +28,48 @@ export default function TripDetail(props) {
         }
     }, [id]);
 
+    const editView = (id) => {
+        Axios.get(`../../trip/edit?id=${id}`)
+        .then((response) => {
+          console.log("Loaded trip information for editing", response.data.trip)
+          var trip = response.data.trip
+          setIsEdit(true)
+          setCurrentTrip(trip)
+        })
+        .catch((error) => {
+          console.log("Error loading trip information for editing")
+          console.log(error)
+        })
+      }
+    
+    const editTrip = (trip) => {
+        Axios.put("../../trip/update", trip)
+        .then((response) => {
+          console.log("Updated trip information")
+        })
+        .catch((error) => {
+          console.log("Error updating trip information")
+          console.log(error)
+        })
+      }
+
     return (
       <div>
           <h4>{currentTrip.title}</h4> by username-here {currentTrip.rating}
           <div>{currentTrip.city}, {currentTrip.country}</div>
           <p>{currentTrip.summary}</p>
+          <button onClick={() => {editView(currentTrip._id)}}>Edit</button>
+
+
+        {
+            (isEdit) ?
+            <Trip key={currentTrip._id} trip={currentTrip} editTrip={editTrip} isEdit={isEdit}/>
+            :
+            <></>
+        }
+
       </div>
+
+ 
     )
 }
