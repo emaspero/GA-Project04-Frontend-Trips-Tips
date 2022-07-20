305beaadfa5521000b2e4edb2d1846cf105bd983
Alexandra <alexandra@kwaltys-air.lan>
2022-07-21 00:24:59 +0200

    added my trips to profile

--- src/trips/BrowseTrips.js
+++ src/trips/BrowseTrips.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import TripSnippet from './TripSnippet';
-// import TripEditForm from './TripEditForm';
+import TripEditForm from './TripEditForm';
 
 export default function BrowseTrips(props) {
   const [trips, setTrips] = useState([]);
   const [currentTrip, setCurrentTrip] = useState("");
-  // const [isEdit, setIsEdit] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
 
   
 
@@ -25,45 +25,45 @@ export default function BrowseTrips(props) {
     })
   };
 
-  // const singleTrip = (id) => {
-  //   Axios.get(`trip/detail/${id}`)
-  //   .then((response) => {
-  //       console.log("Loaded Trip information")
-  //       console.log(response.data.trip)
+  const singleTrip = (id) => {
+    Axios.get(`trip/detail/${id}`)
+    .then((response) => {
+        console.log("Loaded Trip information")
+        console.log(response.data.trip)
 
-  //       var trip = response.data.trip
-  //       setCurrentTrip(trip)
-  //   })
-  //   .catch((error) => {
-  //     console.log("Error loading trip information")
-  //     console.log(error)
-  // })
-  // }
+        var trip = response.data.trip
+        setCurrentTrip(trip)
+    })
+    .catch((error) => {
+      console.log("Error loading trip information")
+      console.log(error)
+  })
+  }
 
-  // const editView = (id) => {
-  //   Axios.get(`trip/edit?id=${id}`)
-  //   .then((response) => {
-  //     console.log("Loaded trip information for editing", response.data.trip)
-  //     var trip = response.data.trip
-  //     setIsEdit(true)
-  //     setCurrentTrip(trip)
-  //   })
-  //   .catch((error) => {
-  //     console.log("Error loading trip information for editing")
-  //     console.log(error)
-  //   })
-  // }
+  const editView = (id) => {
+    Axios.get(`trip/edit?id=${id}`)
+    .then((response) => {
+      console.log("Loaded trip information for editing", response.data.trip)
+      var trip = response.data.trip
+      setIsEdit(true)
+      setCurrentTrip(trip)
+    })
+    .catch((error) => {
+      console.log("Error loading trip information for editing")
+      console.log(error)
+    })
+  }
 
-  // const editTrip = (trip) => {
-  //   Axios.put("trip/update", trip)
-  //   .then((response) => {
-  //     console.log("Updated trip information")
-  //   })
-  //   .catch((error) => {
-  //     console.log("Error updating trip information")
-  //     console.log(error)
-  //   })
-  // }
+  const editTrip = (trip) => {
+    Axios.put("trip/update", trip)
+    .then((response) => {
+      console.log("Updated trip information")
+    })
+    .catch((error) => {
+      console.log("Error updating trip information")
+      console.log(error)
+    })
+  }
 
   const allTrips = trips.map((trip, index) => (
     <div key={index}>
