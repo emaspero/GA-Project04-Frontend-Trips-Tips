789cd07dfef230083be835618667e1d082fcd4bb
Alexandra <alexandra@kwaltys-air.lan>
2022-07-22 16:42:15 +0200

    getting some css updated

--- src/trips/TripSnippet.js
+++ src/trips/TripSnippet.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Axios from 'axios';
+import './TripSnippet.css';
 
 export default function TripSnippet(props) {
   const [liked, setLiked] = useState();
@@ -63,28 +64,38 @@ export default function TripSnippet(props) {
 
   return (
     <>
-      <h4>{props.title}</h4> by {props.createdBy.username} &nbsp; {props.rating}{" "}
-      &nbsp;
-      {!liked ? (
-        <img
-          src="/img/heart_empty.png"
-          alt="empty heart"
-          id="emptyHeart"
-          onClick={handleClick}
-        ></img>
-      ) : (
-        <img
-          src="/img/heart_full.png"
-          alt="full heart"
-          id="emptyHeart"
-          value={likedBy}
-        ></img>
-      )}
-      <br />
-      {props.country.name}, {props.city}
-      <p>{props.summary}</p>
-      <Link to={`/trip/detail/${props._id}`}>+ see more</Link>
-      <hr />
+    <div className="trip-container">
+      <div className="trip-container-left">
+        <div className="trip-container-title" >
+          <h4>{props.title}</h4>
+          <div>by @{props.createdBy.username}</div>  
+          <img src={`./img/${props.rating}-star.png`} alt='star' className='rating-star'></img>
+          {!liked ? (
+            <img
+              src="/img/heart_empty.png"
+              alt="empty heart"
+              id="emptyHeart"
+              onClick={handleClick}
+            ></img>
+          ) : (
+            <img
+              src="/img/heart_full.png"
+              alt="full heart"
+              id="emptyHeart"
+              value={likedBy}
+            ></img>
+          )}
+        </div>
+        <br />
+        <p>{props.city}, {props.country.name}</p>
+        <p>{props.summary}</p>
+        <p ><Link to={`/trip/detail/${props._id}`} className="trip-see-more">+ see more</Link></p>
+      </div>
+      <div className="trip-container-right">
+        <div className="trip-container-right-img"><img src="./img/paris.jpg" alt="paris" className="trip-img"></img></div>
+      </div>
+    </div>
+      <hr/>
     </>
   );
 }
